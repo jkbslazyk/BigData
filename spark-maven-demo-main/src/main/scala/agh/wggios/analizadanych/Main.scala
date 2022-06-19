419512c572112ad0836e60b3d51e55d898c4ea73
jkbslazyk <jakubslazyk@gmail.com>
2022-06-19 22:33:53 +0200

    lab13-logs

--- spark-maven-demo-main/src/main/scala/agh/wggios/analizadanych/Main.scala
+++ spark-maven-demo-main/src/main/scala/agh/wggios/analizadanych/Main.scala
@@ -3,11 +3,14 @@ import agh.wggios.analizadanych.session._
 import agh.wggios.analizadanych.datareader.DataReader
 import agh.wggios.analizadanych.datawriter.DataWriter
 import agh.wggios.analizadanych.transformation.transformation
+import org.apache.log4j.Logger
 
 
 object Main extends sparksession{
 
   def main(args: Array[String]): Unit = {
+    lazy val logger:Logger=Logger.getLogger(getClass.getName)
+    logger.info("Komunikat")
     val df = new DataReader("data/2015-summary.csv").read()
     df.show()
     val filtred_df = df.filter(row => new transformation().USflights(row))
